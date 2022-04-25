3e5f7dacc2a0258f647eb3a1f5e589ec1a2c7c69
bapatlasravan <103932090+bapatlasravan@users.noreply.github.com>
2022-04-25 18:51:20 +0530

    Add files via upload

---
+++ print_statements.py
@@ -0,0 +1,23 @@
+"""
+Day 01
+Date: 18th April 2022 Monday
+Name: Bapatla Sravan
+File Desc: Print statements
+"""
+
+print('hello Welcome to Python Programming language') #hello Welcome to Python Programming language
+print("hello Welcome to Python Programming language") #hello Welcome to Python Programming language
+print('''hello Welcome to Python Programming language''') #hi i am Bapatla Sravan
+print("""hello Welcome to Python Programming language""") #hi i am Bapatla Sravan
+print('hello Welcome to Python Programming language"Traing') #hello Welcome to Python Programming language"Traing
+print("hello Welcome to Python Programming language'Traing") #hello Welcome to Python Programming language'Traing
+print('hello Welcome to Python Programming language\'Traing') #hello Welcome to Python Programming language'Traing
+print("hello Welcome to Python Programming language\"Traing") #hello Welcome to Python Programming language"Traing
+print('hi \
+i am \
+Bapatla Sravan') #hi i am Bapatla Sravan
+
+print('''hi i' am" Bapatla Sravan''' ) #hi i' am" Bapatla Sravan
+print("""hi i' am" Bapatla Sravan""" ) #hi i' am" Bapatla Sravan
+print('''hi i\''' am""" Bapatla Sravan''') #hi i''' am""" Bapatla Sravan
+print("""hi i\""" am''' Bapatla Sravan""") #hi i""" am''' Bapatla Sravan
